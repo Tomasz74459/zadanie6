b401cc4265fd3607f3ee24a4e0a3fef2c17c236a
Tomasz74459 <166233708+Tomasz74459@users.noreply.github.com>
2024-04-06 17:13:47 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string sourceFilePath = "test2.txt";
+        string targetFilePath = "test3.txt";
+
+        try
+        {
+            using (FileStream sourceStream = File.OpenRead(sourceFilePath))
+            {
+                using (FileStream targetStream = File.Create(targetFilePath))
+                {
+                    sourceStream.CopyTo(targetStream);
+                }
+            }
+
+            Console.WriteLine("Plik został skopiowany pomyślnie.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd podczas kopiowania pliku: {ex.Message}");
+        }
+    }
+}
